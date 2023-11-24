c96f9c30d061affed4d51883b495527f90c0fa00
Rajpratim <rajpratim.001@gmail.com>
2023-11-24 13:44:42 +0530

    initial setup with tailwindcss

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 const express = require('express');
 const { Sequelize } = require('sequelize');
-
+require('dotenv').config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* Database Connection*/
+const sequelize = new Sequelize(
+  'hit_mentoring_DB',
+  'root',
+  process.env.DB_PASSWORD,
+  {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  }
+);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+  });
+
+// design file
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
 
-// Database Connection//
-// const sequelize = new Sequelize(
-//   'hit_mentoring_DB',
-//   'root',
-//   process.env.DB_PASSWORD,
-//   {
-//     host: '127.0.0.1',
-//     dialect: 'mysql',
-//   }
-// );
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch((error) => {
-//     console.error('Unable to connect to the database: ', error);
-//   });
-
-/*server code*/
-
-const user = {
-  firstName: 'Tim',
-  lastName: 'Cook',
-};
-
+// routers
 app.get('/', (req, res) => {
-  res.render('pages/index', { user: user });
+  res.render('index');
 });
 
-app.listen(2000);
+// server listening
+app.listen(PORT, () => {
+  console.log(`The app start on http://localhost:${PORT}`);
+});
