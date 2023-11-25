728f1c913540e6a0ef3ff4f3111bfaec247e4a3e
Rajpratim121 <82226010+Rajpratim121@users.noreply.github.com>
2023-11-25 20:24:34 +0530

    Rajpratim (#1)
    * added login page
    
    * login page initial work

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /* Database Connection*/
-const sequelize = new Sequelize(
-  'hit_mentoring_DB',
-  'root',
-  process.env.DB_PASSWORD,
-  {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  }
-);
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the database: ', error);
-  });
+// const sequelize = new Sequelize(
+//   'hit_mentoring_DB',
+//   'root',
+//   process.env.DB_PASSWORD,
+//   {
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+//   }
+// );
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch((error) => {
+//     console.error('Unable to connect to the database: ', error);
+//   });
 
 // design file
 app.use(express.static('public'));
@@ -32,7 +32,7 @@ app.set('view engine', 'ejs');
 
 // routers
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('login');
 });
 
 // server listening
